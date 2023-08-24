2705153dc6e139441d4ce3e8510f2d127d9fe9e5
Martin Halford <martin@benext.io>
2023-08-24 15:51:45 +1000

    Initial commit
    Signed-off-by: Martin Halford <martin@benext.io>

---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS Cognito using Rust
+
+Example of using the Cognito AdminInitiateAuth in Rust
---
+++ src/main.rs
@@ -0,0 +1,135 @@
+use std::io;
+use std::io::Write;
+
+use aws_sdk_cognitoidentityprovider::{
+    error::SdkError,
+    operation::admin_initiate_auth::{AdminInitiateAuthError, AdminInitiateAuthOutput},
+    types::AuthFlowType,
+};
+use clap::Parser;
+use colored::*;
+use rpassword::read_password;
+use serde_json::Value;
+
+// ...
+
+//
+// Example of how to make an AWS Cognito call and retrieve a JWT in Rust
+//
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // This example uses the AdminUserPasswordAuth flow which needs a Cognito user pool, a
+    // app client id, a username, and a password.
+    // The userpool and the client id are stored in a JSON file that looks like:
+    //
+    // {
+    // 	"user_pool_id": "ap-southeast-XXXXXXXXXXXX",,
+    // 	"client_id": "YYYYYYYYYY"
+    // }
+    //
+    // This example assumes Windows and its HOMEPATH env variable
+    //
+    // let config_path = env::var("HOMEPATH").unwrap() + "\\.aws\\cognito_pool.json";
+    let config_path = ".".to_string() + "/cognito_pool.json";
+    println!("Config Path: {}", config_path);
+
+    // // Get the JSON config and assign to variables for use
+    let config_text = read_cognito_config(&config_path);
+
+    let user_pool_id = config_text["user_pool_id"].as_str().unwrap();
+    let client_id = config_text["client_id"].as_str().unwrap();
+
+    // Parse the command line args, if username and/or password are not supplied, they will be queried from the user
+    let args = Args::parse();
+
+    // Prompt user for username and password if not on the command line
+    let mut username = args.username;
+    if "NONE".eq(&username) {
+        username = get_username_or_password("Username: ", false);
+    }
+
+    let mut password = args.password;
+    if "NONE".eq(&password) {
+        password = get_username_or_password("Password: ", true);
+    }
+
+    // Call Cognito with username/password and return an AdminInitiateAuthOutput result
+    let cognito_result = make_cognito_call(&username, &password, user_pool_id, client_id).await;
+
+    // Print the id_token field from the result
+    match cognito_result {
+        Ok(result) => {
+            println!(
+                "JWT:\n{}",
+                result
+                    .authentication_result()
+                    .unwrap()
+                    .id_token()
+                    .unwrap()
+                    .blue()
+            );
+        }
+        Err(e) => {
+            eprintln!("An error occurred while authenticating: {:?}", e);
+            return Err(e.into());
+        }
+    }
+
+    Ok(())
+}
+
+async fn make_cognito_call(
+    username: &str,
+    password: &str,
+    user_pool_id: &str,
+    client_id: &str,
+) -> Result<AdminInitiateAuthOutput, SdkError<AdminInitiateAuthError>> {
+    //
+    // The AdminInitiateAuth API required AWS credentials.  In this case the default credentials from .aws are used
+    //
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_cognitoidentityprovider::Client::new(&config);
+
+    client
+        .admin_initiate_auth()
+        .user_pool_id(user_pool_id)
+        .client_id(client_id)
+        .auth_flow(AuthFlowType::AdminUserPasswordAuth)
+        .auth_parameters("USERNAME", username)
+        .auth_parameters("PASSWORD", password)
+        .send()
+        .await
+}
+
+fn get_username_or_password(prompt: &str, is_password: bool) -> String {
+    if !is_password {
+        let mut line = String::new();
+        print!("{}", prompt);
+        io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut line).unwrap();
+        (&line.trim()).to_string()
+    } else {
+        print!("{}", prompt);
+        io::stdout().flush().unwrap();
+        read_password().unwrap()
+    }
+}
+
+fn read_cognito_config(filename: &str) -> Value {
+    let config_text = std::fs::read_to_string(filename).unwrap();
+    serde_json::from_str::<Value>(&config_text).unwrap()
+}
+
+// Struct to hold parsed command line args
+#[derive(Parser, Debug)]
+#[command(version)]
+struct Args {
+    /// Username to login
+    #[arg(short, long, default_value = "NONE")]
+    username: String,
+
+    /// Password
+    #[arg(short, long, default_value = "NONE")]
+    password: String,
+}
